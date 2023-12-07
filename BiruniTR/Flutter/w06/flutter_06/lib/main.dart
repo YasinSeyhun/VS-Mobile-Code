81992ff5eb70ac49789009b7598d2295bdb43a2f
YasinSeyhun <seyhunyasin@gmail.com>
2023-12-08 00:06:48 +0300

    y

--- BiruniTR/Flutter/w06/flutter_06/lib/main.dart
+++ BiruniTR/Flutter/w06/flutter_06/lib/main.dart
@@ -1,1 +1,9 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_06/01_noncontainer_widgets.dart';
+import 'package:flutter_06/02_container_widgets.dart';
 
+
+
+void main(List<String> args) {
+  runApp(Sayfa2());
+}
