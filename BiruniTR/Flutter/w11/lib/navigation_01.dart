c514e0125cd79141132c2e9fdc7806a2d3e224d8
YasinSeyhun <seyhunyasin@gmail.com>
2024-01-20 01:20:07 +0300

    u

--- BiruniTR/Flutter/w11/lib/navigation_01.dart
+++ BiruniTR/Flutter/w11/lib/navigation_01.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 
 class Navigation01MainPage extends StatelessWidget {
+  int user_id = 15;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,12 +12,20 @@ class Navigation01MainPage extends StatelessWidget {
       ),
       body: Center(
         child: ElevatedButton(
-            onPressed: () {
-              Navigator.push(context, MaterialPageRoute(
+            onPressed: () async {
+              var respondedData =
+                  await Navigator.push(context, MaterialPageRoute(
                 builder: (context) {
-                  return Navigation01Page1();
+                  return Navigation01Page1(user_id);
                 },
               ));
+              // Geçişin sonucu respondedData değişkeninde saklanır.(Instance of 'Future<dynamic>' : Retrived)
+              // Future asenkron methoddur.Asenkronlar methodlar beklenmez.Yani 16.satırdakş kod beklenmeden 23.satıra geçiş yapar.
+              print(respondedData.toString() + ": Retrived");
+              //print mesajı page 1 den mainpage geçiş yaparken çağrılır.
+
+              ScaffoldMessenger.of(context)
+                  .showSnackBar(SnackBar(content: Text(respondedData)));
             },
             child: Text("Go to Page 1")),
       ),
@@ -23,21 +33,56 @@ class Navigation01MainPage extends StatelessWidget {
   }
 }
 
+// yapıcı method kullanarak main sayfasından page1 sayfasına veri(id) gönderdik.
 class Navigation01Page1 extends StatelessWidget {
+  int user_id;
+  Navigation01Page1(this.user_id);
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.purpleAccent,
-        title: Text("Navigation-01 Page 1"),
-      ),
-      body: Center(
-        child: ElevatedButton(
-            onPressed: () {
-              Navigator.pop(context);
-            },
-            child: Text("Back")),
+    return WillPopScope(
+      onWillPop: () => CheckStatus(context),
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.purpleAccent,
+          title: Text("Navigation-01 Page 1 ---- user_id : ${user_id}"),
+          titleTextStyle:
+              TextStyle(color: const Color.fromARGB(179, 228, 228, 228)),
+        ),
+        body: Center(
+          child: ElevatedButton(
+              onPressed: () async {
+                await CheckStatus(context);
+                Navigator.maybePop(context, "Kullanıcı bilgileri güncellendi");
+              },
+              child: Text("Back")),
+        ),
       ),
     );
+  }
+
+  Future<bool> CheckStatus(BuildContext context) async {
+    var shouldPop = await showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Bilgileri Eksiksiz Giriniz!"),
+          actions: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: Text("Tamam"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: Text("Yinede Çık"),
+            ),
+          ],
+        );
+      },
+    );
+    return shouldPop;
   }
 }
