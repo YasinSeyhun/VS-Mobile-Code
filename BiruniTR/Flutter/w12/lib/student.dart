d54c0ba3604eadf141aeed0b199eddd5036ba775
YasinSeyhun <seyhunyasin@gmail.com>
2024-01-23 21:05:48 +0300

    u

---
+++ BiruniTR/Flutter/w12/lib/student.dart
@@ -0,0 +1,14 @@
+class Student {
+  int id;
+  String firstName;
+  String lastName;
+  int departman_id;
+
+//json = key value
+
+  Student(
+      {required this.id,
+      required this.firstName,
+      required this.lastName,
+      required this.departman_id});
+}
